d20716914e2438328bd8f801116460c3aa2a9e42
Ariel Verdejo <verdejo.ariel@gmail.com>
2021-12-15 22:24:42 -0300

    Agrego parte serie

--- Raspbery-Publisher/test_mosquito.py
+++ Raspbery-Publisher/test_mosquito.py
@@ -1,7 +1,17 @@
+#! /usr/bin/env python3
+
+import serial
+import socket
+
 import random
 import time
 
 from paho.mqtt import client as mqtt_client
+
+#Variables
+
+#host = "192.168.0.101"
+#port = 8050
 
 broker = 'test.mosquitto.org'
 port = 1883
@@ -50,6 +60,48 @@ def new_publish():
     msg = lista
     publish.multiple(msg, broker)
 """
+"""
+def enviar_datos(mensaje):
+    #Creación de un objeto socket (lado cliente)
+    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #Conexión con el servidor. Parametros: IP (puede ser del tipo 192.1$
+    clientSocket.connect((host, port))
+    print("Conectado al servidor")
+    #Creamos un bucle para retener la conexion
+    #Instanciamos una entrada de datos para que el cliente pueda enviar$
+    #mens = raw_input("Mensaje desde Cliente a Servidor >> ")
+    #for dato in mensaje:
+    #Con el método send, enviamos el mensaje
+    clientSocket.send(mensaje.encode())
+    #Cerramos la instancia del objeto servidor
+    clientSocket.close()
+    #Imprimimos la palabra Adios para cuando se cierre la conexion
+    print("Conexión cerrada")
+"""
+
+def serial_red():
+    ser = serial.Serial('/dev/ttyACM0', 115200)  # open serial port
+    print(ser.name)                              # check which port was r$
+    lista_de_datos = []
+    datos_a_enviar = []
+    AC0 = "000318"                               # chequeo si el Sensor r$
+    while 1:
+        line = ser.readline().decode(encoding = "utf-8")     # write a stri$
+        new_line = (line[:-2])
+        if (new_line == AC0):
+            i = 4
+            while i >= 0:
+                comando = ser.readline().decode(encoding = "utf-8")  # Esto deb$
+                new_comando = comando[:-2]
+                print(new_comando)
+                line = ser.readline().decode(encoding = "utf-8")     # write a $
+                new_line = (line[:-2])
+                lista_de_datos = new_line.split("+")
+                for datos in lista_de_datos:
+                    datos_a_enviar.append(datos)
+                print(datos_a_enviar)
+                i = i - 1
+
 def run():
     client = connect_mqtt()
     client.loop_start()
