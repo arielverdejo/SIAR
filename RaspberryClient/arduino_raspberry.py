7ad334365f5cb39d5acc527856a3799608986fe3
Ariel Verdejo <verdejo.ariel@gmail.com>
2021-12-28 23:41:57 -0300

    Nuevo programa comunicacion Arduino-Raspberry

---
+++ RaspberryClient/arduino_raspberry.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+import serial
+import socket
+
+#Variables
+host = '192.168.0.104'
+port = 8050
+
+
+def enviar_datos(mensaje):
+  # Creación de un objeto socket (lado cliente)
+  clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  # Conexión con el servidor. Parametros: IP (puede ser del tipo 192.168.1.1 o localhost), Puerto
+  clientSocket.connect((host, port))
+  print("Conectado al servidor")
+  clientSocket.send(mensaje.encode())
+  # Cerramos la instancia del objeto servidor
+  clientSocket.close()
+  # Imprimimos mensaje  para cuando se cierre la conexion
+  print("Conexión cerrada")
+
+# Open serial port
+serie = serial.Serial('/dev/ttyACM0', 115200)
+# Check port
+print(serie.name)
+
+lista_de_datos = []
+datos_a_enviar = []
+mensaje = ""
+sequence = 0
+# Variable utilizada para saber si el sensor responde a AC0
+AC0 = "000318"
+
+while 1:
+  # Read a line of Serial Port
+  line = serie.readline().decode(encoding = "utf-8")
+  # Take off the first two elements
+  new_line = (line[:-2])
+  if (new_line == AC0):
+    i = 3
+    while i >= 0:
+      # Read the new line en este caso un comandos
+      comando = serie.readline().decode(encoding = "utf-8")
+      new_comando = comando[:-2]
+      print(new_comando)
+      # Read datos
+      line = serie.readline().decode(encoding = "utf-8")
+      # Quito los primeros datos que no pertenecen a datos utiles
+      new_line = (line[:-2])
+      """
+      lista_de_datos = new_line.split("+")
+      for datos in lista_de_datos:
+        datos_a_enviar.append(datos)
+      print(datos_a_enviar)
+      """
+      print(mensaje)
+      mensaje = mensaje + "+" + new_line
+      i = i - 1
+
+    #mensaje = ";".join(datos_a_enviar)
+    sequence += 1
+    print(sequence)
+    mensaje = str(sequence) + mensaje
+    enviar_datos(mensaje)
+    print(mensaje)
+    #datos_a_enviar = []
+    mensaje = ""
