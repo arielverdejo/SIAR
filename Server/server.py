4b754f42ac946d26e2548856f3cd29f80f704b88
ariel <Ariel Verdejo>
2021-12-31 19:58:12 -0300

    Agrego funcion especial_split

--- Server/server.py
+++ Server/server.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python3
+
 import socket
 import sys
 import mysql.connector
 import time
 import datetime
+import re
 
 def insertDB(listadedatos):
     
@@ -28,13 +31,25 @@ def insertDB(listadedatos):
                             "NorthWindSpeed,EastWindSpeed,dia,hora) "
         "VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
     )
-    data = ("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16",timestamp_day, timestamp_hour)
-    #data = (str(listadedatos[0]),str(listadedatos[1]),str(listadedatos[2]),str(listadedatos[3]),timestamp_day, timestamp_hour)
+    
+    #data = ("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16",timestamp_day, timestamp_hour)
+    data = (str(listadedatos[0]),str(listadedatos[1]),str(listadedatos[2]),str(listadedatos[3]),
+            str(listadedatos[4]),str(listadedatos[5]),str(listadedatos[6]),str(listadedatos[7]),
+            str(listadedatos[8]),str(listadedatos[9]),str(listadedatos[10]),str(listadedatos[11]),
+            str(listadedatos[12]),str(listadedatos[13]),str(listadedatos[14]),str(listadedatos[15]),
+            timestamp_day, timestamp_hour)
     
     cursor.execute(sqlInsertar,data)
     conexion.commit()
     cursor.close()
     conexion.close()
+
+def especial_split(string_to_split):
+    separador = ","
+    string_with_plus = string_to_split.replace("+",",+")
+    string_final = string_with_plus.replace("-",",-")
+    regular_exp = '|'.join(map(re.escape, separador))
+    return re.split(regular_exp, string_final)
 
 def server_conexion():
     port = 8050
@@ -60,7 +75,8 @@ def server_conexion():
             # Receive the data in small chunks and retransmit it
             data = connection.recv(1024).decode("utf-8")
             print('Received: \n{!r}'.format(data))
-            datos_en_lista = [1,2,3,4]
+            datos_en_lista = especial_split(data)
+            #datos_en_lista = [1,2,3,4]
             insertDB(datos_en_lista)
                 
         finally:
