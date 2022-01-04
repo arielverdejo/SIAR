fa782977cb4363cd64a1aaf624e6a63a590720b8
ariel <Ariel Verdejo>
2022-01-04 00:09:21 -0300

    pylint style guide

--- Server/server.py
+++ Server/server.py
@@ -1,52 +1,58 @@
 #!/usr/bin/env python3
 
 import socket
-import sys
-import mysql.connector
 import time
 import datetime
 import re
+import mysql.connector
 
-def insertDB(listadedatos):
-    
-    dbConnect = {
+def insert_database(lista_de_datos):
+    """This function insert a list into the database."""
+    database_connect = {
         "host":"localhost",
         "user":"root",
         "password":"ariel",
         "database":"testDB"
     }
 
-    ts = time.time()
-    timestamp_day = datetime.datetime.fromtimestamp(ts)
+    time_stamp = time.time()
+    timestamp_day = datetime.datetime.fromtimestamp(time_stamp)
     timestamp_hour = datetime.datetime.utcnow()
 
-    conexion = mysql.connector.connect(**dbConnect)
+    conexion = mysql.connector.connect(**database_connect)
     cursor = conexion.cursor()
 
-    sqlInsertar = (
-        "INSERT INTO tester(solar,precipitation,strikes,strikesDistance,"
-                            "windSpeed,windDirection,gustWindSpeed,"
-                            "airTemperature,vaporPressure,atmosphericPressure,relativeHumidity,humiditySensorTemperature,"
-                            "xOrientation,yOrientation,"
-                            "NorthWindSpeed,EastWindSpeed,"
-                            "dia,hora) "
+    sql_insert = (
+        "INSERT INTO tester("
+        "solar,precipitation,"
+        "strikes,strikesDistance,"
+        "windSpeed,windDirection,gustWindSpeed,"
+        "airTemperature,vaporPressure,atmosphericPressure,"
+        "relativeHumidity,humiditySensorTemperature,"
+        "xOrientation,yOrientation,"
+        "NorthWindSpeed,EastWindSpeed,"
+        "dia,hora) "
 
         "VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
     )
-    
-    data = (str(listadedatos[2]),str(listadedatos[3]),str(listadedatos[4]),str(listadedatos[5]),
-            str(listadedatos[7]),str(listadedatos[8]),str(listadedatos[9]),
-            str(listadedatos[11]),str(listadedatos[12]),str(listadedatos[13]),str(listadedatos[14]),str(listadedatos[15]),
-            str(listadedatos[17]),str(listadedatos[18]),
-            str(listadedatos[21]),str(listadedatos[22]),
-            timestamp_day, timestamp_hour)
-    
-    cursor.execute(sqlInsertar,data)
+    data = (
+        str(lista_de_datos[2]),str(lista_de_datos[3]),
+        str(lista_de_datos[4]),str(lista_de_datos[5]),
+        str(lista_de_datos[7]),str(lista_de_datos[8]),str(lista_de_datos[9]),
+        str(lista_de_datos[11]),str(lista_de_datos[12]),str(lista_de_datos[13]),
+        str(lista_de_datos[14]),str(lista_de_datos[15]),
+        str(lista_de_datos[17]),str(lista_de_datos[18]),
+        str(lista_de_datos[21]),str(lista_de_datos[22]),
+        timestamp_day, timestamp_hour
+    )
+    cursor.execute(sql_insert,data)
     conexion.commit()
     cursor.close()
     conexion.close()
 
 def especial_split(string_to_split):
+    """Convert a string with + and - to a list with element, which conservates
+    the sign + or -."""
     separador = ","
     string_with_plus = string_to_split.replace("+",",+")
     string_final = string_with_plus.replace("-",",-")
@@ -54,6 +60,7 @@ def especial_split(string_to_split):
     return re.split(regular_exp, string_final)
 
 def server_conexion():
+    """This function makes the connection with the server."""
     port = 8050
 
     # Create a TCP/IP socket
@@ -61,7 +68,7 @@ def server_conexion():
 
     # Bind the socket to the port
     server_address = ("", port)
-    print('Starting up on {} port {}'.format(*server_address))
+    print(F"Starting up on {server_address[0]} port {server_address[1]}")
     sock.bind(server_address)
 
     # Listen for incoming connections
@@ -76,18 +83,16 @@ def server_conexion():
 
             # Receive the data in small chunks and retransmit it
             data = connection.recv(1024).decode("utf-8")
-            print('Received: \n{!r}'.format(data))
+            print(F"Received: \n{data}")
             datos_en_lista = especial_split(data)
             print(datos_en_lista)
-            #datos_en_lista = [1,2,3,4]
-            insertDB(datos_en_lista)
-                
+            insert_database(datos_en_lista)
         finally:
             # Clean up the connection
             connection.close()
 
 def main():
-  server_conexion()
+    server_conexion()
 
 if __name__ == "__main__":
-  main()
+    main()
