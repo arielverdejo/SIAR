4a3fa6e49c676286890b0d81db7c75e7412cef45
ariel <Ariel Verdejo>
2022-02-09 20:13:51 -0300

    envio archivo von credenciales

---
+++ RaspberryClient/credentials.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+class Claves:
+    """Esta clase almacena las contraseñas y el usuario"""
+    def _init():
+        self.password = '"password"'
+        self.user = '"usuario"'
+
