30fc907c65dbe80742625fbbc97726ff258a50d0
ariel <verdejo.ariel@gmail.com>
2022-08-07 02:42:38 -0300

    Agrego funcion main en calculo_evotransppiracion

--- RaspberryClient/calculo_evotranspiracion.py
+++ RaspberryClient/calculo_evotranspiracion.py
@@ -235,69 +235,6 @@ def insert_database(dato1, dato2):
       cursor.close()
       conexion.close()
 """
-
-valores_obtenidos = calculos_sensor()
-
-Altura =  920                                                                #B
-LatGrad = 33                                                                 #C Datos fijos que se deben buscar
-LatRad = math.pi * LatGrad/180                                               #D
-dl = valores_obtenidos[0]                                                    #E
-dr = 1+0.033*math.cos(2*math.pi/365*dl)                                      #F 
-delta = 0.409*math.sin(2*math.pi/365*dl-1.39)                                #G
-ws = math.acos(-math.tan(LatRad)*math.tan(delta))                            #H
-sen_sen = math.sin(LatRad)*math.sin(delta)                                   #I
-cos_cos = math.cos(LatRad)*math.cos(delta)                                   #J
-Gsc = 0.082                                                                  #K
-Ra = 24*60/math.pi*Gsc*dr*(ws*sen_sen+cos_cos*math.sin(ws))                  #L
-N =24/math.pi*ws                                                             #M
-n = N                                                                        #N cantidad de tiempo con radiacion solar
-Rs_cal = (0.25 + 0.5*(n/N))*Ra                                               #O
-Rs_real = calculo_radiacion_solar()                                          #P
-Rso_despejado = (0.75+2*10**(-5)*Altura)*Ra                                  #Q
-Evaporacion = 0.408*Rs_real                                                  #R
-Sigma =(4.903)*10**(-9)                                                      #S
-Lambda = 2.45                                                                #T
-cp = 1.013*10**(-3)                                                          #U
-Epsilon = 0.622                                                              #V
-PA_Atmos = valores_obtenidos[1]                                              #Y
-Ganma = cp*PA_Atmos/(Epsilon*Lambda)                                         #W
-TM_Atmos = valores_obtenidos[2]                                              #Z
-Tm_Atmos = valores_obtenidos[3]                                              #AA
-PA = (101.3*((((TM_Atmos+Tm_Atmos)/2+273.16)-0.0065*Altura)/
-     ((TM_Atmos+Tm_Atmos)/2+273.16))**5.26)                                  #X
-ea = 2.1                                                                     #AB
-HR_M_Atmos = valores_obtenidos[4]                                            #AC
-HR_m_Atmos = valores_obtenidos[5]                                            #AD
-es_TM = 0.6108*math.e**(17.27*TM_Atmos/(TM_Atmos+237.3))                     #AE
-es_Tm = 0.6108*math.e**(17.27*Tm_Atmos/(Tm_Atmos+237.3))                     #AF
-ea_M = es_TM*HR_m_Atmos                                                      #AG
-ea_m = es_Tm*HR_M_Atmos                                                      #AH
-es_ea = (es_TM+es_Tm)/2-(ea_M+ea_m)/2                                        #AI
-Rs_Rso = Rs_real/Rso_despejado                                               #AJ
-Rnl = (Sigma *((((TM_Atmos+273.16)**4+(Tm_Atmos+273.16)**4))/2)
-      *(0.34-0.14*math.sqrt((ea_M+ea_m)/2))
-      *(1.35*Rs_real/Rso_despejado-0.35))                                    #AK
-Rns = (1-0.23)*Rs_real                                                       #AL
-Rn_calc = Rns-Rnl                                                            #AM
-Rn = Rn_calc*0.408                                                           #AN
-G = 0                                                                        #AO
-AlturaAnem = 2                                                              #AP
-uviento_Atmos = valores_obtenidos[6]                                         #AQ
-uvientocor = uviento_Atmos*(4.87/(math.log(67.8*AlturaAnem-5.42)))           #AR
-Delta_Mayuscula = ((4098*(0.6108*math.e**((17.27*((TM_Atmos+Tm_Atmos)/2)
-                  /((TM_Atmos+Tm_Atmos)/2+237.3)))))
-                  /(((TM_Atmos+Tm_Atmos)/2+237.3)**2))                       #AS
-Eto = ((Rn*(Delta_Mayuscula)/(Delta_Mayuscula+Ganma*(1+0.34*uvientocor)))+
-      ((900/((TM_Atmos+Tm_Atmos)/2+273)*uvientocor*(es_ea)*Ganma)
-      /(Delta_Mayuscula+Ganma*(1+0.34*uviento_Atmos))))                      #AT
-
-
-Parametros = ([Altura,LatGrad,LatRad,dl,dr,delta,ws,sen_sen,cos_cos,Gsc,Ra,N,n,
-               Rs_cal,Rs_real,Rso_despejado,Evaporacion,Sigma,Lambda,cp,Epsilon,
-               Ganma,PA,PA_Atmos,TM_Atmos,Tm_Atmos,ea,HR_M_Atmos,HR_m_Atmos,
-               es_TM,es_Tm,ea_M,ea_m,es_ea,Rs_Rso,Rnl,Rns,Rn_calc,Rn,G,
-               AlturaAnem,uviento_Atmos,uvientocor,Delta_Mayuscula,Eto])
-
 def pasar_csv(lista):
     fields = ["Altura","LatGrad","LatRad","dl","dr","delta","ws","sen_sen",
             "cos_cos","Gsc","Ra","N","n","Rs_cal","Rs_real","Rso_despejado",
@@ -313,17 +250,86 @@ def pasar_csv(lista):
         write.writerow(fields) 
         write.writerow(lista)
 
-pasar_csv(Parametros)
-#impresora(Parametros)
+def obtener_valores_sensor():
+    valores_obtenidos = calculos_sensor()
 
-print("Este es el eto " + str(Eto))
-#insert_database(Eto,1)
+    Altura =  920                                                            #B
+    LatGrad = 33                                                             #C Datos fijos que se deben buscar
+    LatRad = math.pi * LatGrad/180                                           #D
+    dl = valores_obtenidos[0]                                                #E
+    dr = 1+0.033*math.cos(2*math.pi/365*dl)                                  #F 
+    delta = 0.409*math.sin(2*math.pi/365*dl-1.39)                            #G
+    ws = math.acos(-math.tan(LatRad)*math.tan(delta))                        #H
+    sen_sen = math.sin(LatRad)*math.sin(delta)                               #I
+    cos_cos = math.cos(LatRad)*math.cos(delta)                               #J
+    Gsc = 0.082                                                              #K
+    Ra = 24*60/math.pi*Gsc*dr*(ws*sen_sen+cos_cos*math.sin(ws))              #L
+    N =24/math.pi*ws                                                         #M
+    n = N                                                                    #N cantidad de tiempo con radiacion solar
+    Rs_cal = (0.25 + 0.5*(n/N))*Ra                                           #O
+    Rs_real = calculo_radiacion_solar()                                      #P
+    Rso_despejado = (0.75+2*10**(-5)*Altura)*Ra                              #Q
+    Evaporacion = 0.408*Rs_real                                              #R
+    Sigma =(4.903)*10**(-9)                                                  #S
+    Lambda = 2.45                                                            #T
+    cp = 1.013*10**(-3)                                                      #U
+    Epsilon = 0.622                                                          #V
+    PA_Atmos = valores_obtenidos[1]                                          #Y
+    Ganma = cp*PA_Atmos/(Epsilon*Lambda)                                     #W
+    TM_Atmos = valores_obtenidos[2]                                          #Z
+    Tm_Atmos = valores_obtenidos[3]                                          #AA
+    PA = (101.3*((((TM_Atmos+Tm_Atmos)/2+273.16)-0.0065*Altura)/
+        ((TM_Atmos+Tm_Atmos)/2+273.16))**5.26)                               #X
+    ea = 2.1                                                                 #AB
+    HR_M_Atmos = valores_obtenidos[4]                                        #AC
+    HR_m_Atmos = valores_obtenidos[5]                                        #AD
+    es_TM = 0.6108*math.e**(17.27*TM_Atmos/(TM_Atmos+237.3))                 #AE
+    es_Tm = 0.6108*math.e**(17.27*Tm_Atmos/(Tm_Atmos+237.3))                 #AF
+    ea_M = es_TM*HR_m_Atmos                                                  #AG
+    ea_m = es_Tm*HR_M_Atmos                                                  #AH
+    es_ea = (es_TM+es_Tm)/2-(ea_M+ea_m)/2                                    #AI
+    Rs_Rso = Rs_real/Rso_despejado                                           #AJ
+    Rnl = (Sigma *((((TM_Atmos+273.16)**4+(Tm_Atmos+273.16)**4))/2)
+        *(0.34-0.14*math.sqrt((ea_M+ea_m)/2))
+        *(1.35*Rs_real/Rso_despejado-0.35))                                  #AK
+    Rns = (1-0.23)*Rs_real                                                   #AL
+    Rn_calc = Rns-Rnl                                                        #AM
+    Rn = Rn_calc*0.408                                                       #AN
+    G = 0                                                                    #AO
+    AlturaAnem = 2                                                           #AP
+    uviento_Atmos = valores_obtenidos[6]                                     #AQ
+    uvientocor = uviento_Atmos*(4.87/(math.log(67.8*AlturaAnem-5.42)))       #AR
+    Delta_Mayuscula = ((4098*(0.6108*math.e**((17.27*((TM_Atmos+Tm_Atmos)/2)
+                    /((TM_Atmos+Tm_Atmos)/2+237.3)))))
+                    /(((TM_Atmos+Tm_Atmos)/2+237.3)**2))                     #AS
+    Eto = ((Rn*(Delta_Mayuscula)/(Delta_Mayuscula+Ganma*(1+0.34*uvientocor)))+
+        ((900/((TM_Atmos+Tm_Atmos)/2+273)*uvientocor*(es_ea)*Ganma)
+        /(Delta_Mayuscula+Ganma*(1+0.34*uviento_Atmos))))                    #AT
 
-guardar_en_archivo(Eto)
 
-nuevo = Eto_object.EtoObject().read_Eto()
+    Parametros = ([Altura,LatGrad,LatRad,dl,dr,delta,ws,sen_sen,cos_cos,Gsc,Ra,
+                N,n,Rs_cal,Rs_real,Rso_despejado,Evaporacion,Sigma,Lambda,cp,
+                Epsilon,Ganma,PA,PA_Atmos,TM_Atmos,Tm_Atmos,ea,HR_M_Atmos,
+                HR_m_Atmos,es_TM,es_Tm,ea_M,ea_m,es_ea,Rs_Rso,Rnl,Rns,Rn_calc,
+                Rn,G,AlturaAnem,uviento_Atmos,uvientocor,Delta_Mayuscula,Eto])
+    
+    #pasar_csv(Parametros)
+    #impresora(Parametros)
 
-print(nuevo)
+    #print("Este es el eto " + str(Eto))
+    #insert_database(Eto,1)
+
+    guardar_en_archivo(Eto)
+
+    #nuevo = Eto_object.EtoObject().read_Eto()
+    #print(nuevo)
+
+def main():
+  obtener_valores_sensor()
+
+if __name__ == "__main__":
+  main()
+
 """
 Calcular
 - dias dl #E
