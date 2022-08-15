0833a0a5f6f4d93f56f293b11aad74c2c24b9ee4
ariel <verdejo.ariel@gmail.com>
2022-08-15 20:45:36 -0300

    Quitar nueva linea de valor en el archivo

--- RaspberryClient/Eto_object.py
+++ RaspberryClient/Eto_object.py
@@ -4,7 +4,7 @@ class EtoObject:
   def read_Eto(self):
     """Lee un valor desde un archivo y lo devuelve"""
     with open("/home/pi/SIAR/RaspberryClient/valor_Eto") as f:
-      valor = f.splitlines()
+      valor = f.readline().splitlines()
     return valor
 
   def saludar(self):
