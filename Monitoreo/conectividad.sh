f23011983fcf349033a354fa577c43ad7c79cb73
ariel <verdejo.ariel@gmail.com>
2022-08-18 00:09:37 -0300

    Modifico ubicacion de archivo Logs porque se borraban en tmp

--- Monitoreo/conectividad.sh
+++ Monitoreo/conectividad.sh
@@ -2,18 +2,18 @@
 netcat -z -w 10 siardb.frm.utn.edu.ar 3306
 if [ $? -eq 0 ]
 then
-        echo -n `date '+%Y-%m-%d %H:%M > '` >> /tmp/connectivity.log
-        echo " OK    conectividad a siardb" >> /tmp/connectivity.log
+        echo -n `date '+%Y-%m-%d %H:%M > '` >> /home/pi/Documents/Logs/connectivity.log
+        echo " OK    conectividad a siardb" >> /home/pi/Documents/Logs/connectivity.log
 else
-        echo -n `date '+%Y-%m-%d %H:%M > '` >> /tmp/connectivity.log
-        echo " ERROR conectividad a siardb" >> /tmp/connectivity.log
+        echo -n `date '+%Y-%m-%d %H:%M > '` >> /home/pi/Documents/Logs/connectivity.log
+        echo " ERROR conectividad a siardb" >> /home/pi/Documents/Logs/connectivity.log
 fi
 netcat -z -w 10 google.com 80
 if [ $? -eq 0 ]
 then
-        echo -n `date '+%Y-%m-%d %H:%M > '` >> /tmp/connectivity.log
-        echo " OK    conectividad a google" >> /tmp/connectivity.log
+        echo -n `date '+%Y-%m-%d %H:%M > '` >> /home/pi/Documents/Logs/connectivity.log
+        echo " OK    conectividad a google" >> /home/pi/Documents/Logs/connectivity.log
 else
-        echo -n `date '+%Y-%m-%d %H:%M > '` >> /tmp/connectivity.log
-        echo " ERROR conectividad a google" >> /tmp/connectivity.log
+        echo -n `date '+%Y-%m-%d %H:%M > '` >> /home/pi/Documents/Logs/connectivity.log
+        echo " ERROR conectividad a google" >> /home/pi/Documents/Logs/connectivity.log
 fi
