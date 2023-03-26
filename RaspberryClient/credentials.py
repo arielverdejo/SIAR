458f6c25ede82f36e2310e6f34cbc2a520a865d5
ariel <verdejo.ariel@gmail.com>
2023-03-26 20:26:42 -0300

    Elimino archivos que no se van a usar

--- RaspberryClient/credentials.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-class Claves:
-    """Esta clase almacena las contraseñas y el usuario"""
-    def __init__(self):
-        self.password = "'contraseña'"
-        self.user = "'usuario'"
-
